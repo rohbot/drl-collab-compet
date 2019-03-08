e23951848c8cb4264fab664d9008ea8cae13341a
Rohan Ahmad <rohanoahmad@gmail.com>
2019-03-08 16:47:28 +0700

    renamed params in training file

--- train.py
+++ train.py
@@ -13,18 +13,20 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.distributions import Normal
-from mqtt_writer import SummaryWriter
+from tensorboardX import SummaryWriter
 from tqdm import tqdm
 
 ENV_FILE            = "./Tennis_Linux_NoVis/Tennis.x86_64"
-HIDDEN_SIZE         = 512
-LEARNING_RATE       = 1e-4
+
+
 GAMMA               = 0.97
-GAE_TAU             = 0.95
-PPO_CLIP            = 0.2
-PPO_STEPS           = 2000
+LAMBDA              = 0.95
+BUFFER_SIZE         = 2000
 MINI_BATCH_SIZE     = 200
-PPO_EPOCHS          = 10
+PPO_CLIP            = 0.2
+NUM_EPOCHS          = 10
+LEARNING_RATE       = 1e-4
+HIDDEN_SIZE         = 512
 
 TARGET_REWARD       = 0.5
 
@@ -32,9 +34,8 @@ TARGET_REWARD       = 0.5
 if __name__ == "__main__":    
 
     env = UnityEnvironment(file_name=ENV_FILE)
-    writer = SummaryWriter()
-    writer.reset()
-    agent = Agent(env, HIDDEN_SIZE, LEARNING_RATE, GAMMA, GAE_TAU, PPO_STEPS, PPO_EPOCHS, MINI_BATCH_SIZE, PPO_CLIP)
+    writer = SummaryWriter(comment="tennis/ppo")
+    agent = Agent(env, HIDDEN_SIZE, LEARNING_RATE, GAMMA, LAMBDA, BUFFER_SIZE, NUM_EPOCHS, MINI_BATCH_SIZE, PPO_CLIP)
     best_reward = None
     all_scores = []
     averages = []
@@ -48,7 +49,6 @@ if __name__ == "__main__":
 
         log_entry = 'Episode %s. reward: %.3f ave: %.3f' % (agent.episode, test_reward, last_average)
         print(log_entry)
-        writer.log(log_entry)
         writer.add_scalar("episode", agent.episode, agent.episode)
         writer.add_scalar("score", test_reward, agent.episode)
         writer.add_scalar("average", last_average, agent.episode)
@@ -56,8 +56,7 @@ if __name__ == "__main__":
         if best_reward is None or best_reward < test_reward:
             if best_reward is not None:
                 log_entry = "Best reward updated: %.3f -> %.3f" % (best_reward, test_reward)
-                print(log_entry)
-                writer.log(log_entry)
+                writer.add_scalar("best_reward", best_reward, agent.episode)
                 name = "%s_best_%+.3f_%d.dat" % ("ppo", test_reward, agent.episode)
                 fname = os.path.join('.', 'checkpoints', name)
                 torch.save(agent.model.state_dict(), fname)
@@ -70,3 +69,4 @@ if __name__ == "__main__":
     #Save all_scores to file for graphing purposes        
     timestamp =  str(int(time.time()))
     pickle.dump( all_scores, open( "all_scores_"+timestamp+".p", "wb" ) )
+    pickle.dump( averages, open( "average_"+timestamp+".p", "wb" ) )
